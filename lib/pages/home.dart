905d6ae9220270cd4c33d078402466cb9a2ceab7
gustavogss <gustavogss.dev@gmail.com>
2023-06-23 00:07:54 -0300

    Menu e Tela de Cadastro

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:crud_app/pages/cadastro.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -10,10 +11,50 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: const Text("Home"),
-          backgroundColor: const Color.fromARGB(255, 135, 6, 209)),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+            title: const Text("Home"),
+            backgroundColor: const Color.fromARGB(255, 103, 58, 183)),
+        drawer: Drawer(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                    child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        width: double.infinity,
+                        child: const Text("Dados cadastrais")),
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Cadastro()));
+                    }),
+                const Divider(),
+                const SizedBox(height: 10),
+                InkWell(
+                    child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        width: double.infinity,
+                        child: const Text("Configurações")),
+                    onTap: () {}),
+                const Divider(),
+                const SizedBox(height: 10),
+                InkWell(
+                    child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        width: double.infinity,
+                        child: const Text("Termos de uso e Privacidade")),
+                    onTap: () {}),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
